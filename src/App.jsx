4841f8ac7ea784fcc0a80f083c470e78ddb68847
thehsd <hosseinialireza485@gmail.com>
2024-11-26 19:08:15 +0330

    add tailwind

--- src/App.jsx
+++ src/App.jsx
@@ -1,118 +1,17 @@
-// import { useEffect, useState } from "react";
-// import "./App.css";
-// // import TodoApp from './TodoApp';
-// // import FlightCard from './FlightCard';
-// // import FlightPrice from './FlightPrice';
-// // import SimpleCard from './SimpleCard';
-// // import ShowStatus from './ShowStatus';
-// import Box from "./Box";
-// // import Button from './Button';
-
-// function App() {
-//   // ===============================||show product
-//   //   const [list,setList]=useState([])
-//   //   async function productList(){
-//   //   let endPoint = "https://fakestoreapi.com/products";
-//   //   let getProduct = await fetch(endPoint);
-//   //   let result = await getProduct.json();
-//   // setList(result);
-//   // console.log(result)
-//   //   }
-//   //   const [toggle,settoggle]=useState(false);
-//   // useEffect(()=>{
-//   //   productList();
-//   // },[toggle])
-//   // ====================================||counter button function
-//   // const [count,setcount]=useState(0);
-//   // const decrease=()=>{
-//   //   if(count>0){
-//   //     setcount(count-1);
-//   //   }
-//   // }
-
-//   const [isOnline, setIsOnline] = useState(true);
-
-//   console.log(window.navigator.onLine);
-
-//   // function updateIsOnline() {
-//   //   setIsOnline(window.navigator.onLine);
-//   // }
-//   // useEffect(() => {
-//   // updateIsOnline();
-//   // }, [window.navigator.onLine]);
-
-//   useEffect(() => {
-//     window.addEventListener("online", () => setIsOnline(true));
-//     window.addEventListener("offline", () => setIsOnline(false));
-//   }, []);
-
-//   return (
-//     <>
-//       {/* ------------------------------||show product */}
-
-//       {/* <Button click={()=>{settoggle(!toggle)}}>
-//   {toggle? "hide Data":"show Data"}
-//   </Button>
-//  {toggle ?
-//   list.map(item =>(
-// <SimpleCard
-// price={item.price}
-// image={item.image}
-// title={item.title}
-// key={item.id}
-// />
-//   )):""
-// }  */}
-//       {/* ----------------------------------|| counter button */}
-//       {/* <Box>
-//   <Button click={decrease}>-</Button>
-//    <span>{count}</span>
-//    <Button click={()=>{setcount(count+1)}}>+</Button>
-// </Box> */}
-//       {/* =========================================|| Online cheking */}
-//       {<Box>{isOnline ? "online" : "offline"}</Box>}
-//     </>
-//   );
-// }
-
-// export default App;
-
-import React, { useEffect, useState } from "react";
-import Box from "./Box";
-import Button from "./Button";
+import "./App.css";
 
 const App = () => {
-  const [quote, setQuote] = useState(null);
-  const [isFetching, setIsFetching] = useState(false);
-
-  const getRandomQuote = async () => {
-    setIsFetching(true);
-    const endPoint = "http://api.quotable.io/random";
-    const request = await fetch(endPoint);
-    const response = await request.json();
-    setQuote(response);
-    setIsFetching(false);
-  };
-  useEffect(() => {
-    getRandomQuote();
-  }, []);
-
   return (
     <>
-      <Button disabled={isFetching} click={getRandomQuote}>
-        {isFetching ? "Fetching...." : "Get Quote"}
-      </Button>
-
-      {quote !== null && (
-        <Box>
-          <q>{quote.content}</q>
-          <br />
-          <br />
-          <i>{quote.author}</i>
-        </Box>
-      )}
+      <h1 className="text-4xl text-blue-700"> this is react app</h1>
+      <button className="btn">click me</button>
+      <div className="rounded-sm border border-stone-950 m-2 p-3 flex justify-between">
+        <div className="bg-emerald-500 p-3  hover:rounded-3xl transition-all">
+          box 1
+        </div>
+        <div className="bg-indigo-800 p-3">box 2</div>
+      </div>
     </>
   );
 };
-
 export default App;
